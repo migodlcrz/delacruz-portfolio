089fc8418d80291eaab1edf5b9abdea60d9f2c85
Lorenzo <lorenzo.cap.delacruz@gmail.com>
2024-07-15 10:20:04 +0800

    add: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import logo from "../assets/black-logo.png";
+import Socials from "../components/Socials";
+
+const Header = () => {
+  const [time, setTime] = useState("");
+
+  const getManilaTime = () => {
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: "Asia/Manila",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    };
+    const manilaTime = new Intl.DateTimeFormat([], options).format(new Date());
+    setTime(manilaTime);
+  };
+
+  useEffect(() => {
+    getManilaTime();
+    const intervalId = setInterval(getManilaTime, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="relative sticky top-0 z-50 bg-white border-b-[0.5px]">
+      <div className="flex flex-row w-full h-24 py-2 px-24">
+        <div className="flex flex-row w-1/2 justify-between space-x-14">
+          <img src={logo} alt="" className="w-auto h-full" />
+          <div className="flex flex-row w-full justify-between items-center poppins">
+            <span className="text-black">Home</span>
+            <span className="text-black">Services</span>
+            <span className="text-black">About Me</span>
+            <span className="text-black">Projects</span>
+          </div>
+        </div>
+        <div className="flex flex-row w-1/2 justify-end items-center space-x-10">
+          <div className="flex flex-col">
+            <span className="text-gray-700">Manila, Philippines</span>
+            <span className="text-black font-bold">{time}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Socials />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
