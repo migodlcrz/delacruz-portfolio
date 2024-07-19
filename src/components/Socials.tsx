5b54091d5bffbba629420ae88c1ae1c4c0ca8a7e
Lorenzo <lorenzo.cap.delacruz@gmail.com>
2024-07-19 12:05:55 +0800

    modify: added href for socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,30 +3,36 @@ import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 const Socials = () => {
   return (
     <div className="absolute top-38 right-4 flex flex-col space-y-2 pt-4">
-      <button
-        className="tooltip tooltip-left flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white"
+      <a
+        href="https://github.com/migodlcrz"
+        target="_blank"
+        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white"
         data-tip="Github"
       >
         <span className="text-black">
           <FaGithub />
         </span>
-      </button>
-      <button
-        className="tooltip tooltip-left flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white"
+      </a>
+      <a
+        href="https://www.linkedin.com/in/lorenzo-miguel-dela-cruz-a95b14246/"
+        target="_blank"
+        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white"
         data-tip="LinkedIn"
       >
         <span className="text-black">
           <FaLinkedin />
         </span>
-      </button>
-      <button
-        className="tooltip tooltip-left flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white"
+      </a>
+      <a
+        href="https://facebook.com/migodlcrz/"
+        target="_blank"
+        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white"
         data-tip="Facebook"
       >
         <span className="text-black">
           <FaFacebook />
         </span>
-      </button>
+      </a>
     </div>
   );
 };
