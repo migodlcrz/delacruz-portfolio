bbf269d53413308a10a322a8ed694b815436242c
Lorenzo <lorenzo.cap.delacruz@gmail.com>
2024-07-19 16:30:34 +0800

    modify: minor changes

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -6,7 +6,7 @@ const Socials = () => {
       <a
         href="https://github.com/migodlcrz"
         target="_blank"
-        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white text-black hover:bg-black hover:text-white transition-colors"
+        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white text-black hover:bg-gray-700 hover:text-white transition-colors"
         data-tip="Github"
       >
         <FaGithub />
@@ -14,7 +14,7 @@ const Socials = () => {
       <a
         href="https://www.linkedin.com/in/lorenzo-miguel-dela-cruz-a95b14246/"
         target="_blank"
-        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white text-black hover:bg-black hover:text-white transition-colors"
+        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white text-black hover:bg-gray-700 hover:text-white transition-colors"
         data-tip="LinkedIn"
       >
         <FaLinkedin />
@@ -22,7 +22,7 @@ const Socials = () => {
       <a
         href="https://facebook.com/migodlcrz/"
         target="_blank"
-        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white text-black hover:bg-black hover:text-white transition-colors"
+        className="tooltip tooltip-left cursor-pointer flex items-center justify-center border-[0.5px] border-gray-400 rounded-full p-2 w-10 h-10 bg-white text-black hover:bg-gray-700 hover:text-white transition-colors"
         data-tip="Facebook"
       >
         <FaFacebook />
