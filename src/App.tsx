dca98d1be7750b3608e83fee41d51089f3021eb9
Lorenzo <lorenzo.cap.delacruz@gmail.com>
2024-07-19 11:54:52 +0800

    modify: added reference for header clicks

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,63 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import "./index.css";
 import Footer from "./components/Footer";
+import { useRef } from "react";
 
 const App = () => {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const servicesRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+
   return (
     <BrowserRouter>
-      <AppContent />
+      <AppContent
+        homeRef={homeRef}
+        aboutRef={aboutRef}
+        servicesRef={servicesRef}
+        projectsRef={projectsRef}
+      />
     </BrowserRouter>
   );
 };
 
-function AppContent() {
+interface AppContentProps {
+  homeRef: React.RefObject<HTMLDivElement>;
+  aboutRef: React.RefObject<HTMLDivElement>;
+  servicesRef: React.RefObject<HTMLDivElement>;
+  projectsRef: React.RefObject<HTMLDivElement>;
+}
+
+const AppContent: React.FC<AppContentProps> = ({
+  homeRef,
+  aboutRef,
+  servicesRef,
+  projectsRef,
+}) => {
   return (
     <div>
-      <Header />
+      <Header
+        homeRef={homeRef}
+        aboutRef={aboutRef}
+        servicesRef={servicesRef}
+        projectsRef={projectsRef}
+      />
       <Routes>
-        <Route path={"/"} element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              homeRef={homeRef}
+              aboutRef={aboutRef}
+              servicesRef={servicesRef}
+              projectsRef={projectsRef}
+            />
+          }
+        />
       </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
