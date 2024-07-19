278ded493310943d198ce934c7e821a042db09a9
Lorenzo <lorenzo.cap.delacruz@gmail.com>
2024-07-19 16:30:43 +0800

    modify: added footer props

--- src/interfaces/ReferenceProps.ts
+++ src/interfaces/ReferenceProps.ts
@@ -13,3 +13,10 @@ export interface HeaderProps {
   servicesRef: RefObject<HTMLDivElement>;
   projectsRef: RefObject<HTMLDivElement>;
 }
+
+export interface FooterProps {
+  homeRef: RefObject<HTMLDivElement>;
+  aboutRef: RefObject<HTMLDivElement>;
+  servicesRef: RefObject<HTMLDivElement>;
+  projectsRef: RefObject<HTMLDivElement>;
+}
